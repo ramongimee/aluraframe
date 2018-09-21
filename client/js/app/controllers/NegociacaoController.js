11a40649280bb3e4a5dd3538f707b9bd3946f5ad
Ramon Gimenes <ramongimee@hotmail.com>
2018-09-21 09:05:21 -0300

    Melhorando Proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,19 +9,11 @@ class NegociacaoController{
 
         let self = this;
 
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
-                get(target, prop,receiver) {
-                    
-                    if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-
-                        return function(){
-                            Reflect.apply(target[prop], target,arguments);
-                            self._negociacoesView.update(target);
-                        }
-
-                    }
-                    return Reflect.get(target,prop,receiver);
-                }
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacao(),
+            ['adiciona','esvazia'],
+            (model) =>{
+                this._negociacoesView.update(model);
             });
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
