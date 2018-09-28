152de7501f40ef2617187bf86a8d22f5fe59060e
Ramon <ramongimee@hotmail.com>
2018-09-27 21:38:50 -0300

    Parâmetros REST

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,21 +7,15 @@ class NegociacaoController{
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self = this;
-
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacao(),
-            ['adiciona','esvazia'],
-            (model) =>{
-                this._negociacoesView.update(model);
-            });
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'))
-        this._mensagemView.update(this._mensagem);
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona','esvazia');
+            
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto');
     }
 
     adiciona(event){
@@ -29,19 +23,15 @@ class NegociacaoController{
         event.preventDefault();
     
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-
         this._limpaFormulario();
     }
 
     apaga(){
 
         this._listaNegociacoes.esvazia();
-
         this._mensagem.texto = "Negociações apagadas com sucesso";
-        this._mensagemView.update(this._mensagem);
+
     }
 
     _criaNegociacao(){
