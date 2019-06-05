704755fa6d38111bbe1974df132636800cf8649b
Ramon Gimenes <ramongimee@hotmail.com>
2019-06-05 14:47:42 -0300

    Simplificando requisiçoes Ajax com a Fetch Api

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,66 +1,24 @@
 class HttpService{
 
+    _handleErrors(res){
+        if(!res.ok)throw new Error(res.statusText);
+        return res;
+    }
+
     get(url){
 
-        return new Promise((resolve,reject) => {
-
-            let xhr = new XMLHttpRequest();
-
-            /* configurações */
-            xhr.open('GET',url);
-    
-            /* Configurações */
-    
-            xhr.onreadystatechange = () => {
-    
-            /* 
-                0: requisição ainda não iniciada
-    
-                1: conexão com o servidor estabelecida
-    
-                2: requisição recebida
-    
-                3: processando requisição
-    
-                4: requisição está concluída e a resposta está pronta*/
-    
-                if(xhr.readyState == 4){
-                    
-                    if(xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-    
-            /* executa */
-            xhr.send();
-        });
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
     }
     post(url, dado) {
 
-
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-        });
-
+        return fetch(url, {
+            headers: {'Content-type': 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => _handleErrors(res))
+        .then(res => res.json());
     }
 }
