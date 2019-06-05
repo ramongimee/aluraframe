81aa6327f40bb9b56618470eff9903dfacb97d50
Ramon Gimenes <ramongimee@hotmail.com>
2019-06-05 15:12:57 -0300

    Tornando nosso código ainda mais compatível usando babel

---
+++ client/js/app-es6/models/Mensagem.js
@@ -0,0 +1,17 @@
+class Mensagem{
+
+     // DEIXOU DE RECEBER O PARÂMTRO OPCIONAL
+    constructor(texto){
+        this._texto = texto || ''; // se texto for undefined, vai passar ''
+    }
+
+    get texto(){
+
+        return this._texto;
+    }
+
+    set texto(texto){
+
+        this._texto = texto;
+    }
+}
